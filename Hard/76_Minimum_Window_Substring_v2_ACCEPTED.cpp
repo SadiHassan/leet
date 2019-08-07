4b2fdd8a0bca823209ef7a96f55fe77a7acd55d4
sadi.research@gmail.com <sadi.research@gmail.com>
2019-08-06 21:49:41 -0300

    76 - ACCEPTED!!! YAAAYYYYY!!!!!

--- Hard/76_Minimum_Window_Substring_v2_ACCEPTED.cpp
+++ Hard/76_Minimum_Window_Substring_v2_ACCEPTED.cpp
@@ -1,6 +1,3 @@
-#include<iostream>
-using namespace std;
-
 class Solution {
 public:
 
@@ -27,9 +24,10 @@ public:
             p_arr[t[i] - '\0'] += 1;
         }
         int left = 0, right = 0;
+
         while(right < s.size()){
                 s_arr[s[right] - '\0'] += 1;
-                right++;
+
                 if(match_found(p_arr, s_arr)){
                     if(ans > right - left + 1){
                         ans = right - left + 1;
@@ -38,36 +36,21 @@ public:
                     }
                     s_arr[s[left] - '\0'] -= 1;
                     left++;
+                    while(match_found(p_arr, s_arr) && left <= right){
+                        if(ans > right - left + 1){
+                            ans = right - left + 1;
+                            start_ind = left;
+                            end_ind = right;
+                        }
+                        s_arr[s[left] - '\0'] -= 1;
+                        left++;
+                    } //end inner while
                 }
-        }
-        while(left < s.size()){
-            if(match_found(p_arr, s_arr)){
-                    if(ans > right - left + 1){
-                        ans = right - left + 1;
-                        start_ind = left;
-                        end_ind = right;
-                    }
-
-            }
-            s_arr[s[left] - '\0'] -= 1;
-            left++;
+            right++;
         }
 
-        right = s.size() - 1;
-        left = start_ind;
-        cout << left << " " << right << endl;
-        while(right >= 0){
-            if(match_found(p_arr, s_arr)){
-                    if(ans > right - left + 1){
-                        ans = right - left + 1;
-                        start_ind = left;
-                        end_ind = right;
-                    }
 
-            }
-            s_arr[s[right] - '\0'] -= 1;
-            right--;
-        }
+
         string ans_str = "";
         if(start_ind < 0 ) return ans_str;
         for(int i = start_ind; i <= end_ind; i++)
@@ -76,9 +59,3 @@ public:
         return ans_str;
     }
 };
-
-int main(){
-    Solution* p;
-    cout << p->minWindow("AC", "A") << endl;
-    return 0;
-}
